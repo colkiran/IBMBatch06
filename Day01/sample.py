c103aee736784e44df086dbba086e5f122cf2a46
Kiran <colkiran@gmail.com>
2024-07-22 17:57:27 +0530

    Initial commit

---
+++ Day01/sample.py
@@ -0,0 +1,12 @@
+
+#! c:\python311\bin
+
+# this is a comment
+print("Hello World")        # this is a comment
+print("Hello World")
+
+"""
+print("Hello World")
+print("Hello World")
+print("Hello World")
+"""
